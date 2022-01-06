f10388ae146f2de7366c4824ed95b1bb25899290
Alhosainy Altaher <alhosainy.altaher@gmail.com>
2022-01-05 21:08:54 -0500

    Completed my solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  let arrayAfterRemoval = new Array();
+  for (let i = 0; i < arr.length; i++){
+    let skipped = false;
+    
+    for (let k = 0; k < args.length; k++) {
+      if (arr[i] === args[k]) {
+        skipped = true;
+        break;
+      }
+    }
+    
+    if (skipped) {
+      continue;
+    }
 
+    arrayAfterRemoval.push(arr[i]);
+  }
+  return arrayAfterRemoval;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof(num1) != "number" || typeof(num2) != "number") {
+    return 'ERROR';
+  }
+  if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  }
 
+  let sum = 0;
+  let diff = Math.abs(num1 - num2);
+  let count = 1;
+  let smaller = (num1 < num2) ? num1 : num2;
+  
+  sum += num1;
+  sum += num2;
+  while (count < diff) {
+    sum += smaller + count++;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
