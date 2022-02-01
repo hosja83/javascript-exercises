44c8ffa1fda9f2ab1da0b9da9eeeaaaaaeabb379
Alhosainy Altaher <alhosainy.altaher@gmail.com>
2022-02-01 14:10:15 -0500

    Completed excercise using Array.map to translate a cryptography Ceasar Cipher message

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,31 @@
-const caesar = function() {
+const caesar = function(str, shift) {
+  const strArr = str.split("");
+  const newStrArr = strArr.map((ch) => {
+    let charCode = ch.charCodeAt(0);
+    let adjustedShift = shift % 26;
+    let newCharCode = charCode + adjustedShift;
+    if ((charCode >= 65 && charCode < 91) || (charCode >= 97 && charCode < 123)) {
+      if (charCode >= 65 && charCode < 91) {
+        if (newCharCode >= 91) {
+          return String.fromCharCode((newCharCode % 91) + 65);
+        } else if (newCharCode < 65) {
+          return String.fromCharCode(newCharCode + 26);
+        } else {
+          return String.fromCharCode(newCharCode);
+        }
+      } else if (newCharCode >= 123) {
+        return String.fromCharCode((newCharCode % 123) + 97);
+      } else if (newCharCode < 97) {
+        return String.fromCharCode(newCharCode + 26);
+      } else {
+        return String.fromCharCode(newCharCode);
+      }
+    } else {
+      return ch;
+    }
+  });
 
+  return newStrArr.join("");
 };
 
 // Do not edit below this line
