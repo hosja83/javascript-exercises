a37bda1601fea0f0d8697bae02c335dad4fc2452
Alhosainy Altaher <alhosainy.altaher@gmail.com>
2022-02-01 08:24:03 -0500

    Completed using reduce function on array of objects to solve exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  return arr.reduce((oldest, person) => {
+    let currentAge, prevAge;
 
+    //"key" in object tests whether property is undefined in object
+    if ("yearOfDeath" in person === false)
+      person.yearOfDeath = new Date().getFullYear();
+
+    currentAge = person.yearOfDeath - person.yearOfBirth;
+    prevAge = oldest.yearOfDeath - oldest.yearOfBirth;//
+    return currentAge > prevAge ? person : oldest;
+  }, arr[0]);
 };
 
 // Do not edit below this line
